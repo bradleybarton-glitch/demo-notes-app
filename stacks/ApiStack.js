dd5960243434d505eceefb1754a69d6dd4a0034d
Brad Barton <brad.barton@collegeamerica.edu>
2022-04-03 11:12:13 -0600

    Adding a billing API

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -15,6 +15,7 @@ export default class ApiStack extends sst.Stack {
 			defaultFunctionProps: {
 				environment: {
 					TABLE_NAME: table.tableName,
+					STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
 				},
 			},
 		    routes: {
@@ -24,6 +25,7 @@ export default class ApiStack extends sst.Stack {
 				"POST   /notes": "src/create.main",
 				"PUT   /notes/{id}": "src/update.main",
 				"DELETE  /notes/{id}": "src/delete.main",
+				"POST  /billing": "src/billing.main",
 			},
 		});
 
